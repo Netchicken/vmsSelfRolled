9cbd54ecae29dcfd8f0343d6f4305a1cbe4fb998
Gary <Gary.Dix@enform.nz>
2023-10-05 16:09:28 +1300

    build a custom set for initial settings

--- src/components/firebase/DBOperations.js
+++ src/components/firebase/DBOperations.js
@@ -1,128 +1,152 @@
-import React from 'react'
-import { signInWithEmailAndPassword, getAuth, onAuthStateChanged } from "firebase/auth";
-import { doc, updateDoc, getDoc, getFirestore, collection, addDoc, query, where, getDocs, setDoc, documentId } from "firebase/firestore";
-import { auth, db } from '../firebase/Config';
-import { format } from 'date-fns';
+import React from "react";
+import {
+  signInWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+} from "firebase/auth";
+import {
+  doc,
+  updateDoc,
+  getDoc,
+  getFirestore,
+  collection,
+  addDoc,
+  query,
+  where,
+  getDocs,
+  setDoc,
+  documentId,
+} from "firebase/firestore";
+import { auth, db } from "../firebase/Config";
+import { format } from "date-fns";
 import { useState } from "react";
 
 //https://firebase.google.com/docs/firestore/manage-data/add-data
 
 export const SaveToDb = (user) => {
-    const vcUsersRef = collection(db, "vcUsers");
-    const addEntry = setDoc(doc(vcUsersRef, user.uid), {
-        userid: user.uid,
-        lastDateLogin: format(
-            Date.now(),
-            'yyyy-MM-dd HH:MM:SS'
-        ),
-        visitCount: 1
-    });
-    return addEntry
-}
+  const vcUsersRef = collection(db, "vcUsers");
+  const addEntry = setDoc(doc(vcUsersRef, user.uid), {
+    userid: user.uid,
+    lastDateLogin: format(Date.now(), "yyyy-MM-dd HH:MM:SS"),
+    visitCount: 1,
+  });
+  return addEntry;
+};
 
 export const UpdateToDb = (user) => {
-    const docRefUpdate = doc(db, 'vcUsers', user.uid);
-    var visitUpdate = 0;
-    var updateData = {};
-    //get the visitcount
-    getDoc(docRefUpdate).then(docSnap => {
-        if (docSnap.exists()) {
-            const count = docSnap.data().visitCount;
-            visitUpdate = Number(count) + 1;  //get the visitCount and add 1
-            console.log("visitupdate data:", count + " " + docSnap.data().visitCount + 1);
+  const docRefUpdate = doc(db, "vcUsers", user.uid);
+  var visitUpdate = 0;
+  var updateData = {};
+  //get the visitcount
+  getDoc(docRefUpdate).then((docSnap) => {
+    if (docSnap.exists()) {
+      const count = docSnap.data().visitCount;
+      visitUpdate = Number(count) + 1; //get the visitCount and add 1
+      console.log(
+        "visitupdate data:",
+        count + " " + docSnap.data().visitCount + 1
+      );
 
-            updateData = {
-                lastDateLogin: format(
-                    Date.now(),
-                    'yyyy-MM-dd HH:MM:SS'
-                ),
-                visitCount: visitUpdate
-            };
+      updateData = {
+        lastDateLogin: format(Date.now(), "yyyy-MM-dd HH:MM:SS"),
+        visitCount: visitUpdate,
+      };
 
-            const updatedb = updateDoc(docRefUpdate, updateData);
-            console.log("visitupdate updatedb:", updatedb);
+      const updatedb = updateDoc(docRefUpdate, updateData);
+      console.log("visitupdate updatedb:", updatedb);
+    } else {
+      console.log("No such document!");
+    }
+  });
 
-        } else {
-            console.log("No such document!");
-        }
-    })
-
-    // const visitUpdate = 123; // docRefUpdate.data().visitCount + 1;
-    //    return updatedb;
-}
+  // const visitUpdate = 123; // docRefUpdate.data().visitCount + 1;
+  //    return updatedb;
+};
 
 export const checkDataExists = (user) => {
-    // const docRefUpdate = doc(db, 'vcUsers', user.uid);
+  // const docRefUpdate = doc(db, 'vcUsers', user.uid);
 
-    // getDoc(docRefUpdate).then(docSnap => {
-    //     console.log("Document data visitcount:", docSnap.data().visitCount);
-    //     const visitCount = docSnap.data().visitCount;
-    return 1;
-    //})
-}
+  // getDoc(docRefUpdate).then(docSnap => {
+  //     console.log("Document data visitcount:", docSnap.data().visitCount);
+  //     const visitCount = docSnap.data().visitCount;
+  return 1;
+  //})
+};
 export const getData = async (user) => {
-    //https://firebase.google.com/docs/firestore/query-data/queries?hl=en&authuser=0
-    // const q = query(collection(db, "vcUsers"), where("userid", "===", user.id));
-    const q = query(collection(db, "vcUsers"));
-    const querySnapshot = await getDocs(q);
-    if (q) {
-        querySnapshot.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            console.log("DBOperations getData in App  " + doc.id, " => ", doc.data());
-        });
-        return querySnapshot.docs.map(doc => doc.data());
-    }
-    return "No data";
-    //})
-}
-
+  //https://firebase.google.com/docs/firestore/query-data/queries?hl=en&authuser=0
+  // const q = query(collection(db, "vcUsers"), where("userid", "===", user.id));
+  const q = query(collection(db, "vcUsers"));
+  const querySnapshot = await getDocs(q);
+  if (q) {
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log("DBOperations getData in App  " + doc.id, " => ", doc.data());
+    });
+    return querySnapshot.docs.map((doc) => doc.data());
+  }
+  return "No data";
+  //})
+};
 
 export const getBusinessData = async () => {
-    const data = [];
-    const q = query(collection(db, "businessCategories"));
-    const querySnapshot = await getDocs(q);
-    if (q) {
-        querySnapshot.forEach((doc) => {
-
-            console.log("getBusinessData in App  ");
-            data.push(doc.data());
-        });
-        return Promise.all(data);
-    }
-    return "No data";
-}
+  const data = [];
+  const q = query(collection(db, "businessCategories"));
+  const querySnapshot = await getDocs(q);
+  if (q) {
+    querySnapshot.forEach((doc) => {
+      console.log("getBusinessData in App  ");
+      data.push(doc.data());
+    });
+    return Promise.all(data);
+  }
+  return "No data";
+};
 export const getPurposeOfVisitOptionsRef = async () => {
-    const data = [];
-    const q = query(collection(db, "defaultParameters"));
-    const querySnapshot = await getDocs(q);
-    if (q) {
-        querySnapshot.forEach((doc) => {
-            console.log("purposeOfVisitOptionsRef in App  ");
-            data.push(doc.data());
-        });
-        return Promise.all(data);
-    }
-    return "No data";
-
-}
+  const data = [];
+  const q = query(collection(db, "defaultParameters"));
+  const querySnapshot = await getDocs(q);
+  if (q) {
+    querySnapshot.forEach((doc) => {
+      console.log("purposeOfVisitOptionsRef in App  ");
+      data.push(doc.data());
+    });
+    return Promise.all(data);
+  }
+  return "No data";
+};
 export const getDefaultSettingsRef = async () => {
-    const data = [];
-    const q = query(collection(db, "settings-default"));
-    const querySnapshot = await getDocs(q);
-    if (q) {
-        querySnapshot.forEach((doc) => {
-            console.log("defaultSettingsRef in App  ");
-            data.push(doc.data());
-        });
-<<<<<<< HEAD
-        return querySnapshot.docs.map(doc => doc.default);
-=======
-        return Promise.all(data);
->>>>>>> 49cf40ddbbad3a46ebef6e156ff93803a42e2ef7
-    }
-    return "No data";
+  const data = [];
+  const q = query(collection(db, "settings-default"));
+  const querySnapshot = await getDocs(q);
+  if (q) {
+    querySnapshot.forEach((doc) => {
+      console.log("defaultSettingsRef in App  ");
+      data.push(doc.data());
+    });
+    return querySnapshot.docs.map((doc) => doc.default);
+  }
+  return "No data";
+};
 
-}
+// db.collection("settings-zKrDsscyDXN7lQbdujUjjcj3N5K2")
+export const updateSettings = async () => {
+  await setDoc(doc(db, "settings-zKrDsscyDXN7lQbdujUjjcj3N5K2", "init"), {
+    fields: false,
+    primaryColor: "#4A90E2",
+    secondaryColor: "#B8E986",
+    businessCategory: "College",
+    businessName: "Vision College",
+    businessSlogan: "Changing Lives for Learning",
+    businessBranch: "Christchurch",
+    welcomeMessage: "Welcome message in DB",
+    logoImageDownloadUrl: "",
+    backgroundImageDownloadUrl: "",
+    advertsDownloadUrl: "",
+    businessMultiOffices: "",
+    purposeOfVisitOptions: "",
+    createdDate: format(Date.now(), " yyyy-MM-DD HH:MM:SS"),
+  });
+};
 
 // export const userChanged = (user) => {
 
@@ -131,12 +155,10 @@ export const getDefaultSettingsRef = async () => {
 //     const [userData, setUserData] = useState(null);
 //     const [appData, setAppData] = useState(null);
 
-
 //     onAuthStateChanged(auth, user => {
 //         if (user) {
 
 //             //https://firebase.google.com/docs/firestore/quickstart#web-modular-api_1
-
 
 //             const userDataRef = getData(user);
 //             console.log("userDataRef:", userDataRef);
@@ -150,8 +172,6 @@ export const getDefaultSettingsRef = async () => {
 //             // const businessCategoryDataRef = collection(db, "businessCategories");
 //             // const purposeOfVisitOptionsRef = db.collection("defaultParameters").doc("purposeOfVisitOptions");
 //             // const defaultSettingsRef = collection(db, "settings-default"); //.doc("default");
-
-
 
 //             userDataRef.onSnapshot(doc => {
 //                 if (doc.exists) {
@@ -174,13 +194,10 @@ export const getDefaultSettingsRef = async () => {
 //                                         defaultSettings: DefaultSettings
 //                                     });
 
-
-
 //                                     setAauthenticated(true);
 //                                     setCurrentUser(user);
 //                                     setUserData(doc.data());
 //                                     setAppData(AppData);
-
 
 //                                 } else {
 //                                     setAauthenticated(false);
