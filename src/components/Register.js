e62745cb3173256bcdb2aa04bb882b69330c9c51
Gary <Gary.Dix@enform.nz>
2023-10-06 16:45:13 +1300

    fixing

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,38 +1,38 @@
 import React, { useState } from "react";
-// import '../styles/Register.css';
+//import '../styles/Register.css';
 import "../styles/Common.css";
 
 import { auth, db } from "./firebase/Config";
-import { initializeApp, getApp, getApps } from "firebase/app";
+//import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"; //https://firebase.google.com/docs/auth/web/password-auth#create_a_password-based_account
 import { getFirestore, collection, addDoc } from "firebase/firestore";
 
 import { format } from "date-fns";
-
 import NameLogo from "../components/NameLogo";
 
 import {
   TextField,
   Icon,
   InputAdornment,
-  Typography,
   Button,
-  Link,
-  Business,
-  AlternateEmail,
-  Checkbox,
   Divider,
 } from "@mui/material";
+import {
+  Visibility,
+  VisibilityOff,
+  Business,
+  AlternateEmail,
+} from "@mui/icons-material";
 
-import UserDataContext from "../context/userDataContext";
-import { ValidateEmail, ValidatePassword } from "./functions/Validators";
+//import UserDataContext from "../context/userDataContext";
+//import { ValidateEmail, ValidatePassword } from "./functions/Validators";
 import { useNavigate } from "react-router-dom";
 
-export const Register = (props) => {
+const Register = (props) => {
   let navigate = useNavigate();
-  const [businessName, setBusinessName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  let businessName = "";
+  let email = "";
+  let password = "";
   const [showPassword, setShowPassword] = useState(false);
   const [checkedBoxTP, setCheckedBoxTP] = useState(false);
   const [registering, setRegistering] = useState(false);
@@ -57,8 +57,7 @@ export const Register = (props) => {
   };
 
   const checkRegisterInputs = () => {
-    // this.register();
-    // if (this.state.businessName === '') {
+    // if (businessName === '') {
     //     this.setState(state => ({ businessNameError: 'Your Business Name is required' }));
     // } else {
     //     if (this.state.email === '') {
@@ -86,48 +85,42 @@ export const Register = (props) => {
   };
 
   const register = async () => {
-    console.log("register running");
-    const registerEmail = email;
-    const registerPassword = password;
-    const registerBusinessName = businessName;
-    // const registerEmail = "aaa@aaa1.com";
-    // const registerPassword = "123qwe";
-    // const registerBusinessName = "Vision College Test2";
+    if (email !== "" && password !== "" && businessName !== "") {
+      console.log("register running");
+      // const registerEmail = email;
+      // const registerPassword = password;
+      // const registerBusinessName = businessName;
+      // const registerEmail = "aaa@aaa1.com";
+      // const registerPassword = "123qwe";
+      // const registerBusinessName = "Vision College Test2";
 
-    setRegistering(true);
+      setRegistering(true);
 
-    try {
-      //Cloud Firestore creates collections and documents implicitly the first time you add data to the document.
-      //You do not need to explicitly create collections or documents. https://firebase.google.com/docs/firestore/query-data/get-data
-      console.log("registering");
+      try {
+        console.log("registering");
 
-      await createUserWithEmailAndPassword(
-        auth,
-        registerEmail,
-        registerPassword
-      ).then((data) => {
-        addDoc(collection(db, "vcUsers"), {
-          businessName: registerBusinessName,
-          email: registerEmail,
-          ID: data.user.uid,
-          initialSetup: false,
-          user: "Super Admin",
-          createdDate: format(Date.now(), "yyyy-MM-dd HH:MM:SS"),
-          lastLoginDate: format(Date.now(), "yyyy-MM-dd HH:MM:SS"),
-        }).then(() => {
-          setAuthenticated(true);
-          Navigate("/settings");
+        await createUserWithEmailAndPassword(auth,email,password)
+          .then((data) => {
+          addDoc(collection(db, "vcUsers"), {
+            businessName: businessName,
+            email: email,
+            ID: data.user.uid,
+            initialSetup: false,
+            user: "Super Admin",
+            createdDate: format(Date.now(), "yyyy-MM-dd HH:MM:SS"),
+            lastLoginDate: format(Date.now(), "yyyy-MM-dd HH:MM:SS"),
+          }).then(() => {
+            setAuthenticated(true);
+            navigate("/settings");
+          });
         });
-      });
-    } catch (error) {
-      alert(error);
-      console.log("registering catch error = ", error);
-      setBusinessName("");
-      setEmail("");
-      setPassword("");
-      setRegistering(false);
-      setShowPassword(false);
-      setCheckedBoxTP(false);
+      } catch (error) {
+        alert(error);
+        console.log("registering catch error = ", error);
+        setRegistering(false);
+        setShowPassword(false);
+        setCheckedBoxTP(false);
+      }
     }
   };
 
@@ -144,77 +137,71 @@ export const Register = (props) => {
       <div className='register-message-form'>
         <div className='register-form'>
           <div className='register-form-c'>
-            {/* <TextField
-                                className='input'
-                                id='rBusiness'
-                                variant='outlined'
-                                type='businessName'
-                                label='Business Name'
-                                value={this.state.businessName}
-                                onChange={this.handleChange('businessName')}
-                                fullWidth={true}
-                                helperText={this.state.businessNameError}
-                                error={this.state.businessNameError !== ''}
-                                InputProps={{
-                                    endAdornment: (
-                                        <InputAdornment position='end'>
-                                            <Icon className='input-icon'>
-                                                <Business />
-                                            </Icon>
-                                        </InputAdornment>
-                                    ),
-                                }}
-                            /> */}
+            <TextField
+              className='input'
+              id='rBusiness'
+              variant='outlined'
+              type='businessName'
+              label='Business Name'
+              value={businessName}
+              onChange={handleChange(businessName)}
+              fullWidth={true}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position='end'>
+                    <Icon className='input-icon'>
+                      <Business />
+                    </Icon>
+                  </InputAdornment>
+                ),
+              }}
+            />
 
-            {/* <TextField
-                                className='input'
-                                id='rEmail'
-                                variant='outlined'
-                                type='email'
-                                label='Email'
-                                value={this.state.email}
-                                onChange={this.handleChange('email')}
-                                fullWidth={true}
-                                helperText={this.state.emailError}
-                                error={this.state.emailError !== ''}
-                                InputProps={{
-                                    endAdornment: (
-                                        <InputAdornment position='end'>
-                                            <Icon className='input-icon'>
-                                                <AlternateEmail />
-                                            </Icon>
-                                        </InputAdornment>
-                                    ),
-                                }}
-                            /> */}
+            <TextField
+              className='input'
+              id='rEmail'
+              variant='outlined'
+              type='email'
+              label='Email'
+              value={email}
+              onChange={handleChange(email)}
+              fullWidth={true}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position='end'>
+                    <Icon className='input-icon'>
+                      <AlternateEmail />
+                    </Icon>
+                  </InputAdornment>
+                ),
+              }}
+            />
 
-            {/* <TextField
-                                className='input'
-                                id='lPassword'
-                                variant='outlined'
-                                type={this.state.showPassword ? 'text' : 'password'}
-                                label='Password'
-                                value={this.state.password}
-                                onChange={this.handleChange('password')}
-                                fullWidth={true}
-                                helperText={this.state.passwordError}
-                                error={this.state.passwordError !== ''}
-                                InputProps={{
-                                    endAdornment: (
-                                        <InputAdornment position='end'>
-                                            <Icon
-                                                className='input-icon'
-                                                onClick={this.handleClickShowPassword}
-                                            >
-                                                {this.state.showPassword ? <Visibility /> : <VisibilityOff />}
-                                            </Icon>
-                                        </InputAdornment>
-                                    ),
-                                }}
-                            /> */}
+            <TextField
+              className='input'
+              id='lPassword'
+              variant='outlined'
+              type={showPassword ? "text" : "password"}
+              label='Password'
+              value={password}
+              onChange={handleChange(password)}
+              fullWidth={true}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position='end'>
+                    <Icon
+                      className='input-icon'
+                      onClick={handleClickShowPassword}
+                    >
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </Icon>
+                  </InputAdornment>
+                ),
+              }}
+            />
 
-            <div className='register-tnc'>
-              {/* <FormControlLabel
+            {/* <div className='register-tnc'>
+               <FormControlLabel
                                     className='register-tnc-checkbox'
                                     color='primary'
                                     control={
@@ -224,8 +211,8 @@ export const Register = (props) => {
                                             value='checkedBoxTP'
                                         />
                                     }
-                                /> */}
-            </div>
+                                /> 
+            </div> */}
 
             {/* {this.state.checkedBoxTPError ?
                                 <p className='register-tnc-error'>^^^**You have not agreed to our
@@ -245,42 +232,30 @@ export const Register = (props) => {
                                     </Link>
                                     }
 
-                                </AuthTypeContext.Consumer> */}
+                                </AuthTypeContext.Consumer>  */}
 
               <Button
                 className='auth-button'
                 variant='contained'
                 color='primary'
                 size='large'
-                disabled={this.state.registering}
-                onClick={this.checkRegisterInputs}
+                disabled={registering}
+                onClick={register}
               >
-                {this.state.registering ? "Registering....." : "Get Started"}
+                {registering ? "Registering....." : "Get Started"}
               </Button>
             </div>
           </div>
         </div>
       </div>
 
-      <UserDataContext.Provider
+      {/* <UserDataContext.Provider
         value={{
-          authenticated: this.state.authenticated,
+          setAauthenticated(value),
         }}
-      />
+      /> */}
     </div>
   );
 };
 
-//https://reactrouter.com/en/6.4.0/start/faq#what-happened-to-withrouter-i-need-it
-// function withRouter(Component) {
-//   function ComponentWithRouterProp(props) {
-//     let location = useLocation();
-//     let navigate = useNavigate();
-//     let params = useParams();
-//     return <Component {...props} router={{ location, navigate, params }} />;
-//   }
-
-//   return ComponentWithRouterProp;
-// }
-
-// export default withRouter(Register);
+export default Register;
