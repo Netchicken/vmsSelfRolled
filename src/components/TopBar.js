f9ef51179e84b80ed2beb786927b4d793be27dfd
Gary Dix <Gary.Dix@atcltd.atc.org.nz>
2023-10-16 12:01:18 +1300

    formatting css

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -10,52 +10,32 @@ const TopBar = (props) => {
     padding: "10px",
   };
 
-  const authButtons = {
-    textTransform: "none",
-    marginRight: "30px",
-    width: "150px",
-    padding: "10px",
-  };
-
-  const orButton = {
-    textTransform: "none",
-    margin: "0 0 0 -30px",
-  };
-
-  const ppats = props.ppats ? true : false;
-  // console.log("TopBar props.goToRegisterPage", props.goToRegisterPage);
-  // console.log("TopBar props.goToLoginPage", props.goToLoginPage);
 
   return (
     <div>
       <div className='home-topBar'>
         <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-          }}
+          style={{ width: "100px", marginLeft: "20px", cursor: "pointer" }}
+          onClick={props.goToHomePage}
         >
-          <div
-            style={{ width: "200px", marginLeft: "20px", cursor: "pointer" }}
-            onClick={props.goToHomePage}
-          >
-            <NameLogo height='60px' />
-          </div>
-
-          <Button
-            style={consoleButton}
-            variant='contained'
-            color='primary'
-            onClick={props.goToVisitorLoginPage}
-          >
-            Load the Visitor Login Page  {props.businessdata.businessName} {props.businessdata.businessBranch}
-          </Button>
+          <NameLogo height='60px' />
         </div>
+
+
+
+
         <div className='home-title'>
           {props.businessdata.businessName} {props.businessdata.businessBranch}
-
         </div>
+
+        <Button
+          style={consoleButton}
+          variant='contained'
+          color='primary'
+          onClick={props.goToVisitorLoginPage}
+        >
+          Load the Visitor Login Page
+        </Button>
 
         <div>
           <Button
@@ -68,7 +48,7 @@ const TopBar = (props) => {
           </Button>
 
           <Button
-            style={authButtons}
+            style={consoleButton}
             color='primary'
             variant='outlined'
             onClick={props.goToLoginPage}
@@ -76,12 +56,9 @@ const TopBar = (props) => {
             Admin Login
           </Button>
 
-          {/* <Button style={orButton} color='primary' disabled>
-            Or
-          </Button> */}
 
           <Button
-            style={authButtons}
+            style={consoleButton}
             color='primary'
             variant='contained'
             onClick={props.goToRegisterPage}
@@ -89,7 +66,6 @@ const TopBar = (props) => {
             Register for Admin
           </Button>
         </div>
-        {/* )} */}
       </div>
     </div>
   );
