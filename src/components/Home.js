0e81f2924a1398a320a5ea3512b924f45ec92549
Gary Dix <Gary.Dix@atcltd.atc.org.nz>
2023-10-17 13:27:48 +1300

    fixed home css

--- src/components/Home.js
+++ src/components/Home.js
@@ -27,19 +27,17 @@ const Home = () => {
 '>
         <h3 className="home-title">Steps to use this Visitor Management System</h3>
 
-        <div className="list">
+        <p className="ptext">Register your business with "Register For Admin. This creates your business profile and gives you a unique ID"</p>
 
-          <span className="ptext">Register your business with "Register For Admin. This creates your business profile and gives you a unique ID"</span>
+        <p className="ptext">Login in with "Admin Login" on the public machine that will be used. </p>
 
-          <p className="ptext">Login in with "Admin Login" on the public machine that will be used. </p>
+        <p className="ptext">Click on "Visitor Login" to get the VMS running. This shows the login screen for the visitors to fill in</p>
+        <p className="ptext">Check your Stats on "Go to Console"</p>
+        <p className="ptext">Click the logo to get back to this page (no logo on phone)</p>
 
-          <p className="ptext">Click on "Visitor Login" to get the VMS running. This shows the login screen for the visitors to fill in</p>
-          <p className="ptext">Check your Stats on "Go to Console"</p>
-          <p className="ptext">Click the logo to get back to this page</p>
-
-        </div>
       </div>
     </div>
+
 
   );
 };
