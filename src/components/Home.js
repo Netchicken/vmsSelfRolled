cc4930ae3104fb75b421cb068201e3d83164c125
Gary Dix <Gary.Dix@atcltd.atc.org.nz>
2023-10-19 09:19:02 +1300

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -31,7 +31,7 @@ const Home = () => {
         <p className="ptext">Click on "Visitor Login" to get the VMS running. This shows the login screen for the visitors to fill in</p>
         <p className="ptext">Check your Stats on "Go to Console"</p>
         <p className="ptext">Click the logo to get back to this page (no logo on phone)</p>
-        <p className="ptext">Generate a QR code from the menu to log in on your cell phone (under construction)</p>
+        <p className="ptext">Generate a QR code from the menu to log in on your cell phone this QR code is auto generated with the ID of the Business as a parameter (under construction)</p>
         <p className="ptext">NOTE: Viewing the app without logging in will show empty fields and null values. The screens are populated with the details on the Business page.</p>
 
       </div>
